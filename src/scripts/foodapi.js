f3892038ae22540713c5bf5bfafc67a15e129966
Jeff Hill <jeffhill1978@gmail.com>
2019-06-09 20:33:57 -0500

    using open foods API to add items to food list chp10

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,23 +1,49 @@
 // console.log("Hello")
+const foodFactory = function(foodItem) {
+  // Create your own HTML structure for the food list
+  return `<div>
+            <h2>Food:${foodItem.name}</h2>
+            <h3>Ethnicity:${foodItem.ethnicity}</h3>
+            <h4>Category:${foodItem.category}</h4>
+            <li>Barcode:${foodItem.barcode}</li>
+            <li>Ingredients:${foodItem.ingredients}</li>
+            <li>Country:${foodItem.country}</li>
+            <li>Carbs:${foodItem.carbohydrates}</li>
+            <li>Fat:${foodItem.fat}</li>
+            <li>Sugar:${foodItem.sugar}</li>
+
+            </div>`;
+};
+let foodContainer = document.querySelector(".foodList");
 
 fetch("http://localhost:8088/food")
-    .then(foods => foods.json())
-    .then(parsedFoods => {
-        console.table(parsedFoods)
+  .then(foods => foods.json())
+  .then(parsedFoods => {
+    console.table(parsedFoods);
 
-        const foodFactory = function (foodItem) {
-            // Create your own HTML structure for the food list
-            return `<div>
-            <h2>${foodItem.name}</h2>
-            <h3>${foodItem.ethnicity}</h3>
-            <h4>${foodItem.category}</h4>
-            </div>`;
-        }
 
-        let foodContainer = document.querySelector(".foodList");
+    for (let i = 0; i < parsedFoods.length; i++) {
+      // Now fetch the food from the Food API
 
-        for (let i = 0; i < parsedFoods.length; i++) {
-            foodContainer.innerHTML += foodFactory(parsedFoods[i]);
-        }
+      fetch(
+        `https://world.openfoodfacts.org/api/v0/product/${
+          parsedFoods[i].barcode
+        }.json`
+      )
+        .then(response => response.json())
+        .then(productInfo => {
+          console.log(productInfo);
+          parsedFoods[i].ingredients = productInfo.product.ingredients_text;
+          parsedFoods[i].country = productInfo.product.countries;
+          parsedFoods[i].carbohydrates = productInfo.product.nutriments.carbohydrates;
+          parsedFoods[i].fat = productInfo.product.nutriments.fat;
+          parsedFoods[i].sugar = productInfo.product.nutriments.sugars;
+          //   // Produce HTML representation
+          //   const foodAsHTML = foodFactory(parsedFoods[i]);
 
-    })
+          //   // Add representaiton to DOM
+          //   addFoodToDom(foodAsHTML);
+          foodContainer.innerHTML += foodFactory(parsedFoods[i]);
+        });
+    }
+  });
